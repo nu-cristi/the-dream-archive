33fa3e2be59cd64ee99560320cb428ea1c535992
nu-cristi <cristian.dobre98@yahoo.com>
2025-05-14 00:51:09 +0300

    add background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<svelte:head><title>test</title></svelte:head>
+<svelte:head><title>The Dream Archive</title></svelte:head>
 
+<div class="main-container">
+
+</div>
+
+
+<style>
+    .main-container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-image: url('/images/main_background.gif');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
